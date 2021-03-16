349d25c015fba6a3e4f6c85d55f65ece97366fb0
Ricky Bennett <norman.bennett525@gmail.com>
2021-03-16 05:12:27 -0700

    3/16/21

---
+++ README.txt
@@ -0,0 +1,5 @@
+These are my coding solutions for the C# track on exercism.io
+
+This is the link to my profile:
+
+https://exercism.io/profiles/norman-ricky-bennett
---
+++ exercism solutions/Pangram.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class Pangram
+{
+    public static bool IsPangram(string input)
+    {
+        if (string.IsNullOrEmpty(input)) return false;
+
+        var character = 'a';
+        while (character <= 'z')
+        {
+            if (!input.ToLower().Contains(character)) return false;
+            character++;
+        }
+
+        return true;
+    }
+}
